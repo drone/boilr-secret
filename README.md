39a4646ec3a0ee523de5e70503808d9d3873c532
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-09-07 14:14:23 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# boilr-secret
+
+This is a [boilr template](https://github.com/tmrts/boilr) for creating a secret extension. Use a secret extension to expose secrets from external sources. For example, expose secrets from a Vault server. Get started by cloning the project and installing the template:
+
+```console
+$ cd boilr-secret
+$ boilr template save . drone-secret -f
+```
+
+create a project in directory my-secrets:
+
+```console
+$ boilr template use drone-convert my-secrets
+```
+
+enter the docker registry name for this project:
+
+```text
+[?] Please choose a value for "DockerRepository" [default: "foo/bar"]:
+```
+
+enter the go module name:
+
+```text
+[?] Please choose a value for "GoModule" [default: "github.com/foo/bar":
+```
---
+++ template/plugin/plugin.go
@@ -0,0 +1,62 @@
+// Copyright 2019 the Drone Authors. All rights reserved.
+// Use of this source code is governed by the Blue Oak Model License
+// that can be found in the LICENSE file.
+
+package plugin
+
+import (
+	"context"
+	"errors"
+
+	"github.com/drone/drone-go/plugin/secret"
+)
+
+// New returns a new secret plugin.
+func New(param1, param2 string) secret.Plugin {
+	return &plugin{
+		// TODO replace or remove these configuration
+		// parameters. They are for demo purposes only.
+		param1: param1,
+		param2: param2,
+	}
+}
+
+type plugin struct {
+	// TODO replace or remove these configuration
+	// parameters. They are for demo purposes only.
+	param1 string
+	param2 string
+}
+
+func (p *plugin) Find(ctx context.Context, req *secret.Request) (*drone.Secret, error) {
+	// TODO this should be modified or removed. This
+	// demonstrates how we can limit access to secrets
+	// based on repository meta data. For example,
+	// only expose secrets to specific organizations
+	// or accounts.
+	if req.Repo.Namespace != "octocat" {
+		return nil, nil
+	}
+
+	// TODO this should be modified or removed. This
+	// demonstrates how we could return secrets to
+	// pipeline based on name and path.
+	if req.Path = "docker" {
+		switch req.Name {
+		case "username":
+			return &drone.Secret{
+				Name:        "username",
+				Data:        "octocat",
+				PullRequest: false, // never expose to pulls requests
+			}
+		case "password":
+			return &drone.Secret{
+				Name:        "password",
+				Data:        "correct-horse-battery-staple",	
+				PullRequest: false, // never expose to pulls requests
+			}
+		}
+	}
+
+	return nil, nil
+}
